1c4a2f3167c34e9d85fef8e6f8e87a0a2069e9cd
Minjie Fan <mjfan@ucar.edu>
2015-05-17 16:25:26 -0700

    Update log_det_blk_diag.m

--- DFT/log_det_blk_diag.m
+++ DFT/log_det_blk_diag.m
@@ -1,5 +1,5 @@
 function log_det_A = log_det_blk_diag(A)
-%LOG_DET_A   Computes the log determinant of a block diagonal matrix.
+%LOG_DET_BLK_DIAG   Computes the log determinant of a block diagonal matrix.
 %
 %   LOG_DET_A = LOG_DET_BLK_DIAG(A) returns the log determinant of a block
 %   diagonal matrix. A is an N-by-1 cell array of diagonal block matrices.
