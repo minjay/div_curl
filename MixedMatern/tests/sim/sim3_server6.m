d688c2e6bb2481f389ef5879e7a598ebd07cc2ca
minjay <mjfan@ucar.edu>
2016-08-27 21:55:57 -0700

    Edit the script to record the time of running 5000 points

--- MixedMatern/tests/sim/sim3_server6.m
+++ MixedMatern/tests/sim/sim3_server6.m
@@ -2,15 +2,12 @@
 
 clear
 
-savefile = 'MLE2.mat';
-
 % run on server
-parpool(16)
 addpath(genpath('/home/minjay/div_curl'))
 addpath(genpath('/home/minjay/MEALPix'))
 
-n_lat = 15;
-n_lon = 30;
+n_lat = 50;
+n_lon = 100;
 
 % regular grid
 [theta, phi, n] = regular_sampling(n_lat, n_lon);
@@ -25,17 +22,17 @@ p = 2;
 % set the same seed
 rng('default')
 
-N = 500;
+N = 10;
 lb = [0 0 -1 1 1 0 0 0];
 ub = [Inf Inf 1 5 5 Inf Inf Inf];
 
 % specify parameters
 % [sigma1, sigma2, rho12, nu1, nu2, a, tau1, tau2]
 beta_all = [1 1 0.5 3 4 2 0.1 0.1];
-rec_beta_hat = zeros(N, length(beta_all));
 samples_all = mvnrnd(zeros(p*n, 1), eye(p*n), N);
+time_all = zeros(N, 1);
 
-parfor rep = 1:N
+for rep = 1:N
     samples = samples_all(rep, :)';
     [u, v] = sim_mix(n, beta_all, samples, h_mat, r, P_cell, Q_cell, A_cell);
     samples = reshape([u v]', 1, p*n);
@@ -46,13 +43,10 @@ parfor rep = 1:N
     % rand search
     beta_init = rand_search(negloglik1, 100, [0 0 -1 1 1 5 1e-3 1e-3], [5 5 1 5 5 5 1e-3 1e-3], true, false);
     
+    tic
     % fit the model
-    [beta_hat, f_min] = Matern_fit(negloglik1, beta_init, lb, ub, @mycon, false);
-    
-    rec_beta_hat(rep, :) = beta_hat;
+    [beta_hat, f_min] = Matern_fit(negloglik1, beta_init, lb, ub, @mycon, false); 
+    time_all(rep) = toc;
 end
 
-save(savefile, 'rec_beta_hat');
-
-% run on server
-delete(gcp)
+save('time_5000.mat', 'time_all');
