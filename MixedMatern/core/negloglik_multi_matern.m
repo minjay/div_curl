ae6287f198086c82cadb4762a11c0589f304fd11
minjay <mjfan@ucar.edu>
2016-09-23 14:59:52 -0700

    Adapt to the version for multi Matern

--- MixedMatern/core/negloglik_multi_matern.m
+++ MixedMatern/core/negloglik_multi_matern.m
@@ -1,14 +1,13 @@
-function f_value = negloglik_multi_matern(beta_all, h_mat, r, P_cell, Q_cell, A_cell, samples)
+function f_value = negloglik_multi_matern(beta_all, r, samples)
 %NEGLOGLIK   A wrapper of ECMNOBJ to evaluate negative log-likelihood function. 
 %
-%   F_VALUE = NEGLOGLIK(BETA_ALL, H_MAT, R, P_CELL, Q_CELL, A_CELL,
-%   SAMPLES);
+%   F_VALUE = NEGLOGLIK(BETA_ALL, R, SAMPLES);
 %
 % Inputs:
 %   BETA_ALL - the current parameter vector
-%   H_MAT, R, P_CELL, Q_CELL, A_CELL - the outputs of the function
-%   INIT_COMP, which are computed beforehand to speed up the evaluation of
-%   the negative log-likelihood function
+%   R - one of the outputs of the function INIT_COMP, which are computed 
+%   beforehand to speed up the evaluation of the negative log-likelihood 
+%   function
 %   SAMPLES - the T-by-n*p matrix of observations, where T is the number of
 %   replicates, n is the number of sampling locations and p=2
 %
@@ -31,14 +30,17 @@ if abs(rho12)>ub
     beta_all(3) = sign(rho12)*ub;
 end
 
-beta = beta_all(1:6);
-
 disp(['Current estimate of beta is ', mat2str(round(beta_all*1e6)/1e6)])
-[coef, bessel] = get_coef_bessel(beta, r);
+
+sigma1 = beta_all(1);
+sigma2 = beta_all(2);
+a = beta_all(6);
 tau1 = beta_all(7);
 tau2 = beta_all(8);
-cov_mat = get_cov(h_mat, r, P_cell, Q_cell, A_cell, @Matern_mix,...
-    beta, coef, bessel)+diag(kron(ones(1, n), [tau1^2, tau2^2]));
+
+% get cov mat
+cov_mat = get_cov_Matern_pars(r, sigma1, sigma2, rho12, nu1, nu2, a)+...
+    diag(kron(ones(1, n), [tau1^2, tau2^2]));
     
 % negative log-likelihood function
 f_value = ecmnobj(samples, zeros(p*n, 1), cov_mat);
--- MixedMatern/tests/real/real_fit_multi_matern.m
+++ MixedMatern/tests/real/real_fit_multi_matern.m
@@ -8,12 +8,27 @@ load('wind.mat')
 [h_mat, r, P_cell, Q_cell, A_cell] = init_comp(x, y, z, n, theta, phi);
 
 % negative log-likelihood function
-negloglik1 = @(beta_all) negloglik(beta_all, h_mat, r, P_cell, Q_cell, A_cell, samples);
+negloglik1 = @(beta_all) negloglik_multi_matern(beta_all, r, samples);
 
-% rand search
-beta_init = rand_search(negloglik1, 100, [0 0 -1 1 1 10 0 0], [10 10 1 5 5 10 10 10], true, true);
+beta_init = [0.21810846 0.203180249 0.058196367 1.23929461 1.1322247 0.184217146 0.156812964 -0.07977680];
 
-lb = [0 0 -1 1 1 0 0 0];
+beta_all(1:2) = beta_init(6:7);
+beta_all(3) = beta_init(8);
+beta_all(4:5) = beta_init(4:5);
+beta_all(6) = beta_init(3);
+beta_all(7:8) = beta_init(1:2);
+sigma1 = sqrt(beta_all(1));
+sigma2 = sqrt(beta_all(2));
+rho12 = beta_all(3);
+nu1 = beta_all(4);
+nu2 = beta_all(5);
+a = 1/beta_all(6);
+tau1 = beta_all(7);
+tau2 = beta_all(8);
+
+beta_init = [sigma1 sigma2 rho12 nu1 nu2 a tau1 tau2];
+
+lb = [0 0 -1 0 0 0 0 0];
 ub = [Inf Inf 1 5 5 Inf Inf Inf];
 
 % fit the model
