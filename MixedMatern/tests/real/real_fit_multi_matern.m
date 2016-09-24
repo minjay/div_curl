74fe504fbdd0956316f37ea759c4fd43f3a4688e
minjay <mjfan@ucar.edu>
2016-09-23 22:54:29 -0700

    Adapt to bootstrap version

--- MixedMatern/tests/real/real_fit_multi_matern.m
+++ MixedMatern/tests/real/real_fit_multi_matern.m
@@ -5,11 +5,15 @@ addpath(genpath('/home/minjay/div_curl'))
 load('wind.mat')
 load('samples_multi_matern.mat')
 
+savefile = 'boot_multi_matern.mat';
+
+T = 108;
+p = 2;
+% number of bootstrap samples
+B = 200;
+
 % initial computation
 [h_mat, r, P_cell, Q_cell, A_cell] = init_comp(x, y, z, n, theta, phi);
-
-% negative log-likelihood function
-negloglik1 = @(beta_all) negloglik_multi_matern(beta_all, r, samples_all(1:108, :));
 
 beta_init = [0.21810846 0.203180249 0.058196367 1.23929461 1.1322247 0.184217146 0.156812964 -0.07977680];
 
@@ -32,7 +36,22 @@ beta_init = [sigma1 sigma2 rho12 nu1 nu2 a tau1 tau2];
 lb = [0 0 -1 0 0 0 0 0];
 ub = [Inf Inf 1 5 5 Inf Inf Inf];
 
-% fit the model
-[beta_hat, f_min] = Matern_fit(negloglik1, beta_init, lb, ub, @mycon, true);
+samples_all_cell = cell(B, 1);
+for rep = 1:B
+    samples_all_cell{rep} = samples_all((rep-1)*T+1:rep*T, :);
+end
+
+rec_beta_hat = zeros(B, 8);
+
+parfor rep = 1:B  
+    samples = samples_all_cell{rep};
+    negloglik1 = @(beta_all) negloglik_multi_matern(beta_all, r, samples);
+    
+   % fit the model
+   [beta_hat, f_min] = Matern_fit(negloglik1, beta_init, lb, ub, @mycon, false);
+   rec_beta_hat(rep, :) = beta_hat; 
+end
+
+save(savefile, 'rec_beta_hat');
 
 delete(gcp)
