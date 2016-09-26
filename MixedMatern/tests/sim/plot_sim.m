433d676032fd85f93c0dded77bd2346549ae8634
minjay <mjfan@ucar.edu>
2016-09-25 20:14:54 -0700

    Plot 1/a instead of a

--- MixedMatern/tests/sim/plot_sim.m
+++ MixedMatern/tests/sim/plot_sim.m
@@ -8,12 +8,16 @@ rec_beta_hat3 = rec_beta_hat;
 h = figure;
 subplot = @(m,n,p) subtightplot (m, n, p, [0.1 0.075], [0.1 0.05], [0.05 0.02]);
 
-var_names = {'\sigma_{1}', '\sigma_{2}', '\rho_{12}', '\nu_{1}', '\nu_{2}', 'a', '\tau_{1}', '\tau_{2}'};
+var_names = {'\sigma_{1}', '\sigma_{2}', '\rho_{12}', '\nu_{1}', '\nu_{2}', '1/a', '\tau_{1}', '\tau_{2}'};
 lab_cell = {'10*20', '15*30', '20*40'};
 true_values = [1 1 0.5 3 4 2 0.1 0.1];
 for i = 1:8
     subplot(2,4,i)
-    myboxplot([rec_beta_hat1(:,i) rec_beta_hat2(:,i) rec_beta_hat3(:,i)], lab_cell)
+    if i==6
+        myboxplot([1./rec_beta_hat1(:,i) 1./rec_beta_hat2(:,i) 1./rec_beta_hat3(:,i)], lab_cell)
+    else
+        myboxplot([rec_beta_hat1(:,i) rec_beta_hat2(:,i) rec_beta_hat3(:,i)], lab_cell)
+    end
     title(['$$', var_names{i}, '$$'], 'interpreter', 'latex')
     line([0.5 4.5], [true_values(i) true_values(i)], 'Color', 'k', 'LineStyle', '--')
     hline = findobj(gca, 'tag', 'Median');
