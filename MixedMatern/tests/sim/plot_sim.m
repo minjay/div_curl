2ea519f225e28a1ecba63b7e3ca0563a34d7e434
minjay <mjfan@ucar.edu>
2016-09-26 13:48:49 -0700

    Should use 1/a instead of a

--- MixedMatern/tests/sim/plot_sim.m
+++ MixedMatern/tests/sim/plot_sim.m
@@ -10,7 +10,7 @@ subplot = @(m,n,p) subtightplot (m, n, p, [0.1 0.075], [0.1 0.05], [0.05 0.02]);
 
 var_names = {'\sigma_{1}', '\sigma_{2}', '\rho_{12}', '\nu_{1}', '\nu_{2}', '1/a', '\tau_{1}', '\tau_{2}'};
 lab_cell = {'10*20', '15*30', '20*40'};
-true_values = [1 1 0.5 3 4 2 0.1 0.1];
+true_values = [1 1 0.5 3 4 1/2 0.1 0.1];
 for i = 1:8
     subplot(2,4,i)
     if i==6
