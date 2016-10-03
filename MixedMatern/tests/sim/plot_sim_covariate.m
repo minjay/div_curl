89f4554ef7a7af6fa1982b265a6200d0083309e3
minjay <mjfan@ucar.edu>
2016-10-03 15:05:25 -0700

    Adjust the loc of the text in the plot

--- MixedMatern/tests/sim/plot_sim_covariate.m
+++ MixedMatern/tests/sim/plot_sim_covariate.m
@@ -13,13 +13,14 @@ var_names = {'\sigma_{1}', '\sigma_{2}', '\rho_{12}', '\nu_{1}', '\nu_{2}',...
 
 lab_cell = {'10*20', '15*30', '20*40'};
 true_values = [1 1 0.5 3 4 1/2 0.1 0.1  5 5 -10 10 -15 20];
+loc = [0.3 * ones(1, 10) 0.4 0.3 0.4 0.4];
 
 for i = 1:14
     subplot(3, 5, i)
     if i==6
-        myboxplot([1./rec_beta_hat1(:,i) 1./rec_beta_hat2(:,i) 1./rec_beta_hat3(:,i)], lab_cell, 0.4)
+        myboxplot([1./rec_beta_hat1(:,i) 1./rec_beta_hat2(:,i) 1./rec_beta_hat3(:,i)], lab_cell, loc(i))
     else
-        myboxplot([rec_beta_hat1(:, i) rec_beta_hat2(:, i) rec_beta_hat3(:, i)], lab_cell, 0.4)
+        myboxplot([rec_beta_hat1(:, i) rec_beta_hat2(:, i) rec_beta_hat3(:, i)], lab_cell, loc(i))
     end
     title(['$$', var_names{i}, '$$'], 'interpreter', 'latex')
     line([0.5 4.5], [true_values(i) true_values(i)], 'Color', 'k', 'LineStyle', '--')
