93a824a1c97503763088076d9dd1282f59dd1467
minjay <mjfan@ucar.edu>
2016-10-24 14:26:37 -0700

    Fix a bug of the CRPS evaluation function.

--- MixedMatern/tests/real/CRPS.m
+++ MixedMatern/tests/real/CRPS.m
@@ -3,8 +3,8 @@ function value = CRPS( x, mu, sigma_sq )
 
 sigma = sqrt(sigma_sq);
 x0 = (x-mu)./sigma;
-cdf_x0 = normcdf(x0, mu, sigma);
-pdf_x0 = normpdf(x0, mu, sigma);
+cdf_x0 = normcdf(x0, 0, 1);
+pdf_x0 = normpdf(x0, 0, 1);
 value = sigma.*(x0.*(2*cdf_x0-1)+2*pdf_x0-1/sqrt(pi));
 
 end
