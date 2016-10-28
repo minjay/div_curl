a00aa8e33a01d16396494b8ce0533b09307bb6ff
minjay <mjfan@ucar.edu>
2016-10-28 11:24:01 -0700

    Adjust it to 2 boxplots

--- MixedMatern/tests/sim/plot_sim_healpix.m
+++ MixedMatern/tests/sim/plot_sim_healpix.m
@@ -1,22 +1,20 @@
-load('MLE1.mat')
-rec_beta_hat1 = rec_beta_hat;
-load('MLE2.mat')
+load('MLE_healpix.mat')
 rec_beta_hat2 = rec_beta_hat;
-load('MLE7.mat')
-rec_beta_hat3 = rec_beta_hat;
+load('MLE_healpix2.mat')
+rec_beta_hat1 = rec_beta_hat(1:100, :);
 
 h = figure;
 subplot = @(m,n,p) subtightplot (m, n, p, [0.1 0.075], [0.1 0.05], [0.05 0.02]);
 
 var_names = {'\sigma_{1}', '\sigma_{2}', '\rho_{12}', '\nu_{1}', '\nu_{2}', '1/a', '\tau_{1}', '\tau_{2}'};
-lab_cell = {'10*20', '15*30', '20*40'};
+lab_cell = {'192', '768'};
 true_values = [1 1 0.5 3 4 1/2 0.1 0.1];
 for i = 1:8
     subplot(2,4,i)
     if i==6
-        myboxplot([1./rec_beta_hat1(:,i) 1./rec_beta_hat2(:,i) 1./rec_beta_hat3(:,i)], lab_cell)
+        myboxplot([1./rec_beta_hat1(:,i) 1./rec_beta_hat2(:,i)], lab_cell)
     else
-        myboxplot([rec_beta_hat1(:,i) rec_beta_hat2(:,i) rec_beta_hat3(:,i)], lab_cell)
+        myboxplot([rec_beta_hat1(:,i) rec_beta_hat2(:,i)], lab_cell)
     end
     title(['$$', var_names{i}, '$$'], 'interpreter', 'latex')
     line([0.5 4.5], [true_values(i) true_values(i)], 'Color', 'k', 'LineStyle', '--')
