76ebc5a63b98031da7a038b7cac75056efd31c71
minjay <mjfan@ucar.edu>
2017-03-24 11:17:56 -0700

    Return handle of line plot when i=1.

--- MixedMatern/core/boxplot_curve.m
+++ MixedMatern/core/boxplot_curve.m
@@ -1,10 +1,14 @@
-function boxplot_curve(r_upper, cov_u_upper, nb, col, y_range)
+function lh = boxplot_curve(r_upper, cov_u_upper, nb, col, y_range)
 
 [X_MED,Y_MED,Y_LOW,Y_HIGH] = binned_plot(r_upper, cov_u_upper, nb, 'y_range', y_range);
 
 hold on
 for i = 1:nb
-    line([X_MED(i), X_MED(i)], [Y_LOW(i), Y_HIGH(i)], 'Color', col, 'LineWidth', 1)
+    if i==1
+        lh = line([X_MED(i), X_MED(i)], [Y_LOW(i), Y_HIGH(i)], 'Color', col, 'LineWidth', 1);
+    else
+        line([X_MED(i), X_MED(i)], [Y_LOW(i), Y_HIGH(i)], 'Color', col, 'LineWidth', 1)
+    end
 end
 
 end
