12b2c4938a5c9fe91d0ead105d93fb19b243520c
minjay <mjfan@ucar.edu>
2017-03-24 16:53:05 -0700

    Reduce the margin.

--- MixedMatern/tests/sim/compute_sim_ratio.m
+++ MixedMatern/tests/sim/compute_sim_ratio.m
@@ -1,4 +1,5 @@
 % compute the stderr ratios
+clear
 
 stderr_boot = zeros(10, 8);
 load('MLE2.mat')
@@ -14,9 +15,11 @@ for i = 1:10
     ratio(i, :) = stderr_boot(i, :)./stderr_emp;
 end
 
+subplot = @(m,n,p) subtightplot (m, n, p, [0.1 0.075], [0.05 0.03], [0.05 0.02]);
+subplot(1, 1, 1)
 var_names = {'$$\sigma_{1}$$', '$$\sigma_{2}$$', '$$\rho_{12}$$', '$$\nu_{1}$$', '$$\nu_{2}$$', '$$a$$', '$$\tau_{1}$$', '$$\tau_{2}$$'};
 boxplot(ratio, 'Labels', var_names)
-ylabel('bootstrap stderr/empirical stderr')
+ylabel('Ratio')
 set(gca, 'TickLabelInterpreter', 'latex');
 set(gca, 'FontSize', 12);
 
