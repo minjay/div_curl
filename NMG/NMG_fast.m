6db5d7f85bb1cd9ea646b5829fea07ddea5de94e
minjay <mjfan@ucar.edu>
2017-03-26 11:02:04 -0700

    Move h_deriv_fast outside of the for loop.

--- NMG/NMG_fast.m
+++ NMG/NMG_fast.m
@@ -9,10 +9,11 @@ h_sqrt = a*r;
 M_nu_minus_2 = fun_M(h_sqrt, nu-2);
 M_nu_minus_1 = fun_M(h_sqrt, nu-1);
 
+[h1, h2, h3, h12, h13, h23, h33] = h_deriv_fast(a, h10, h20, h30,...
+    h120, h130, h230, h330);
+
 for i = 1:2
     for j = 1:2
-        [h1, h2, h3, h12, h13, h23, h33] = h_deriv_fast(a, h10, h20, h30,...
-            h120, h130, h230, h330);
         ai = a_vec(i);
         aj = a_vec(j);
         bi = b_vec(i);
