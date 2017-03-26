ce2c07e787ef83d1240a5a10ead968756b61c24d
minjay <mjfan@ucar.edu>
2017-03-25 20:04:45 -0700

    Add a special case when x=0.

--- NMG/fun_M.m
+++ NMG/fun_M.m
@@ -1,6 +1,10 @@
 function value = fun_M(x, nu)
 % compute function M_nu at x
 
-value = x^nu*besselk(nu, x);
+if x>0
+    value = x^nu*besselk(nu, x);
+else
+    value = gamma(nu)/(2^(1-nu));
+end
 
 end
