181d5c651ba8ac420b25c4c3dea442340576f0fb
minjay <mjfan@ucar.edu>
2017-03-27 08:47:54 -0700

    Correct the name.

--- MixedMatern/core/Matern_fit.m
+++ MixedMatern/core/Matern_fit.m
@@ -27,11 +27,11 @@ disp(['The initial guess of beta is ', mat2str(round(beta_init*1e6)/1e6)])
 
 if ~paral
     options = optimoptions(@fmincon, 'Algorithm', 'interior-point',...
-        'Hessian', 'lbfgs', 'Display', 'iter', 'TolX', 1e-6, 'MaxFunctionEvaluations', 1e4);
+        'Hessian', 'lbfgs', 'Display', 'iter', 'TolX', 1e-6, 'MaxFunEvals', 1e4);
 else
     options = optimoptions(@fmincon, 'Algorithm', 'interior-point',...
         'Hessian', 'lbfgs', 'Display', 'iter', 'TolX', 1e-6,...
-        'UseParallel', 'always', 'MaxFunctionEvaluations', 1e4);
+        'UseParallel', 'always', 'MaxFunEvals', 1e4);
 end
 
 [beta_hat, f_min] = fmincon(negloglik1, beta_init, [], [], [], [], lb, ub,...
