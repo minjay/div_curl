052cfc5f7dcff6707eda71246659d7a37c82a8b7
minjay <mjfan@ucar.edu>
2017-03-27 08:37:44 -0700

    Use better initial values.

--- MixedMatern/tests/real/real_fit_NMG_Matern_all.m
+++ MixedMatern/tests/real/real_fit_NMG_Matern_all.m
@@ -1,5 +1,5 @@
 % run on server
-parpool(16)
+parpool(8)
 addpath(genpath('/home/minjay/div_curl'))
 
 load('wind.mat')
@@ -7,11 +7,26 @@ load('wind.mat')
 % initial computation
 [r, h0_cell] = init_comp_NMG(n, theta, phi, x, y, z);
 
+param_BM = [0.21810846 0.203180249 0.058196367 1.23929461 1.1322247 0.184217146 0.156812964 -0.07977680];
+beta_all(1:2) = param_BM(6:7);
+beta_all(3) = param_BM(8);
+beta_all(4:5) = param_BM(4:5);
+beta_all(6) = param_BM(3);
+beta_all(7:8) = param_BM(1:2);
+sigma1 = sqrt(beta_all(1));
+sigma2 = sqrt(beta_all(2));
+rho12 = beta_all(3);
+nu1 = beta_all(4);
+nu2 = beta_all(5);
+a = 1/beta_all(6);
+tau1 = beta_all(7);
+tau2 = beta_all(8);
+
 % negative log-likelihood function
-negloglik1 = @(beta_all) negloglik_NMG_Matern(beta_all, r, samples, h0_cell);
+negloglik1 = @(beta_all) negloglik_NMG_Matern_all(beta_all, r, samples, h0_cell);
 
 % [a1 a2 b1 b2 nu a sigma1 sigma2 w1 w2 tau1 tau2]
-beta_init = [0.090899 -0.056781 -0.00133 0.047889 2.735421 17.183203 0.429205 0.395996 1.239295 1.132225 0.218108 0.20318];
+beta_init = [0.006925 -0.003031 0.000483 0.005205 3.305484 a sigma1 sigma2 nu1 nu2 tau1 tau2];
 negloglik1(beta_init)
 
 % to avoid identifiability problem, set a1>0
