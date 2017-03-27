d1277fd43b22d4ed7e2ce02aadc30ee27bacc9eb
minjay <mjfan@ucar.edu>
2017-03-27 10:59:41 -0700

    Adapt to three models.

--- MixedMatern/tests/real/extract_cov.m
+++ MixedMatern/tests/real/extract_cov.m
@@ -1,4 +1,6 @@
-function [phi_diff, cov_data_sub, cov_sub, lat1, lat2] = extract_cov(i1, i2, cov_data, cov, lat, lon)
+function [phi_diff, cov_data_sub, cov_sub_TMM, cov_sub_BM, cov_sub_NMG,...
+    lat1, lat2] = extract_cov(i1, i2, cov_data, cov_TMM, cov_BM, cov_NMG,...
+    lat, lon)
 % function to extract a subset of covariance matrix
 
 lat_unique = unique(lat);
@@ -9,7 +11,9 @@ index1 = find(lat==lat1);
 index2 = find(lat==lat2);
 
 cov_data_sub = [];
-cov_sub = [];
+cov_sub_TMM = [];
+cov_sub_BM = [];
+cov_sub_NMG = [];
 phi_diff = [];
 
 for i = 1:length(index1)
@@ -18,7 +22,9 @@ for i = 1:length(index1)
         jj = index2(j);
         % extract the pairs on the specified latitudes
         cov_data_sub = [cov_data_sub cov_data(ii, jj)];
-        cov_sub = [cov_sub cov(ii, jj)];
+        cov_sub_TMM = [cov_sub_TMM cov_TMM(ii, jj)];
+        cov_sub_BM = [cov_sub_BM cov_BM(ii, jj)];
+        cov_sub_NMG = [cov_sub_NMG cov_NMG(ii, jj)];
         phi_diff = [phi_diff lon(ii)-lon(jj)];
     end
 end
