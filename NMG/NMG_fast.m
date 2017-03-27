92d28efd58c0175d40fbf6515e28e93b3a3d1a03
minjay <mjfan@ucar.edu>
2017-03-26 19:39:47 -0700

    Use one vector variable for all the h's.

--- NMG/NMG_fast.m
+++ NMG/NMG_fast.m
@@ -1,5 +1,4 @@
-function mat = NMG_fast(r, a_vec, b_vec, nu, a, h10, h20, h30, h120, h130,...
-    h230, h330)
+function mat = NMG_fast(r, a_vec, b_vec, nu, a, h0_vec)
 
 %   R - the Euclidean distance between two locations (theta1, phi1) and
 %   (theta2, phi2)
@@ -9,8 +8,16 @@ h_sqrt = a*r;
 M_nu_minus_2 = fun_M(h_sqrt, nu-2);
 M_nu_minus_1 = fun_M(h_sqrt, nu-1);
 
-[h1, h2, h3, h12, h13, h23, h33] = h_deriv_fast(a, h10, h20, h30,...
-    h120, h130, h230, h330);
+% h_vec = [h1 h2 h3 h12 h13 h23 h33];
+% mutiply a^2 to update since for h0_vec, a=1
+h_vec = a^2*h0_vec;
+h1 = h_vec(1);
+h2 = h_vec(2);
+h3 = h_vec(3);
+h12 = h_vec(4);
+h13 = h_vec(5);
+h23 = h_vec(6);
+h33 = h_vec(7);
 
 for i = 1:2
     for j = 1:2
@@ -18,6 +25,7 @@ for i = 1:2
         aj = a_vec(j);
         bi = b_vec(i);
         bj = b_vec(j);
+        % obtain Gamma coefficients
         [Gamma1, Gamma2] = coef_Gamma(ai, aj, bi, bj, h1, h2, h3, h12, h13, h23, h33);
         mat(i, j) = Gamma1*M_nu_minus_2+Gamma2*M_nu_minus_1;
     end
