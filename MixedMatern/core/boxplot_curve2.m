844e6937ad4db1fc25ab4bab539b54df9602d11f
minjay <mjfan@ucar.edu>
2017-03-31 19:57:41 -0700

    Copy boxplot_curve.

---
+++ MixedMatern/core/boxplot_curve2.m
@@ -0,0 +1,14 @@
+function lh = boxplot_curve2(r_upper, cov_u_upper, nb, col, y_range)
+
+[X_MED,Y_MED,Y_LOW,Y_HIGH] = binned_plot(r_upper, cov_u_upper, nb, 'y_range', y_range);
+
+hold on
+for i = 1:nb
+    if i==1
+        lh = line([X_MED(i), X_MED(i)], [Y_LOW(i), Y_HIGH(i)], 'Color', col, 'LineWidth', 1);
+    else
+        line([X_MED(i), X_MED(i)], [Y_LOW(i), Y_HIGH(i)], 'Color', col, 'LineWidth', 1)
+    end
+end
+
+end
