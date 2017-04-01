51ae9ccb93f35949abd7cbeece3b8da87c3e6f2e
minjay <mjfan@ucar.edu>
2017-03-31 17:41:26 -0700

    Add first bootstrap for NMG.

--- MixedMatern/tests/real/real_bootstrap_NMG.m
+++ MixedMatern/tests/real/real_bootstrap_NMG.m
@@ -1,4 +1,5 @@
 % compute the standard errors by bootstrapping
+% on Hilbert
 clear
 
 % run on server
@@ -6,8 +7,9 @@ parpool(16)
 addpath(genpath('/home/minjay/div_curl'))
 
 load('wind.mat')
+load('samples_NMG.mat')
 
-savefile = 'boot_NMG.mat';
+savefile = 'boot_NMG1.mat';
 
 T = 108;
 p = 2;
@@ -18,28 +20,6 @@ B = 200;
 [r, h0_cell] = init_comp_NMG(n, theta, phi, x, y, z);
 
 beta_all = [0.016283 -0.004629 0.002649 0.011662 2.867271 14.523555 0.270156 0.343079 0.720569 0.830202 0.212515 0.194964];
-a1 = beta_all(1);
-a2 = beta_all(2);
-b1 = beta_all(3);
-b2 = beta_all(4);
-nu = beta_all(5);
-a = beta_all(6);
-sigma1 = beta_all(7);
-sigma2 = beta_all(8);
-w1 = beta_all(9);
-w2 = beta_all(10);
-tau1 = beta_all(11);
-tau2 = beta_all(12);
-cov_mat = get_cov_NMG(r, a1, a2, b1, b2, nu, a, h0_cell)+...
-    get_cov_Matern_pars(r, sigma1, sigma2, 0, w1, w2, a)+...
-    diag(kron(ones(1, n), [tau1^2, tau2^2]));
-
-samples_all = mvnrnd(zeros(p*n, 1), cov_mat, B*T);
-
-samples_all_cell = cell(B, 1);
-for rep = 1:B
-    samples_all_cell{rep} = samples_all((rep-1)*T+1:rep*T, :);
-end
 
 beta_init = beta_all;
 % to avoid identifiability problem, set a1>0
@@ -48,7 +28,7 @@ ub = [Inf Inf  Inf  Inf  5 Inf Inf Inf 5 5 Inf Inf];
 
 rec_beta_hat = zeros(B, 12);
 
-parfor rep = 1:B
+parfor rep = 1:70
     
     samples = samples_all_cell{rep};
     
