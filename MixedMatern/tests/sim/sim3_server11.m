88c1fd7953191fd8beb49b4f9ccb1f988390b627
minjay <mjfan@ucar.edu>
2017-04-03 07:54:00 -0700

    Make the field rougher but keep its variability.

--- MixedMatern/tests/sim/sim3_server11.m
+++ MixedMatern/tests/sim/sim3_server11.m
@@ -1,8 +1,9 @@
 % MLE of the Mixed Matern model
+% nu2 = 3, sigma2 = sqrt(2/3)
 
 clear
 
-savefile = 'MLE1.mat';
+savefile = 'MLE11.mat';
 
 % run on server
 parpool(8)
@@ -31,7 +32,7 @@ ub = [Inf Inf 1 5 5 Inf Inf Inf];
 
 % specify parameters
 % [sigma1, sigma2, rho12, nu1, nu2, a, tau1, tau2]
-beta_all = [1 1 0.5 3 4 2 0.1 0.1];
+beta_all = [1 sqrt(2/3) 0.5 3 3 2 0.1 0.1];
 rec_beta_hat = zeros(N, length(beta_all));
 samples_all = mvnrnd(zeros(p*n, 1), eye(p*n), N);
 
