d117568f34c1be511eb44fa0b1047039a1b053ee
minjay <mjfan@ucar.edu>
2017-04-13 22:26:22 -0700

    Draw a botplot of the difference.

--- MixedMatern/tests/real/real_pred_res_test.m
+++ MixedMatern/tests/real/real_pred_res_test.m
@@ -11,13 +11,17 @@ res_NMG = [MSPE_u MSPE_v MAE_u MAE_v LogS_u LogS_v CRPS_u CRPS_v];
 n = size(res_TMM, 2);
 p_TMM_NMG = zeros(n, 1);
 for i = 1:n
-    [h, p] = ttest2(res_TMM(:, i), res_NMG(:, i));
+    [h, p] = ttest(res_TMM(:, i), res_NMG(:, i));
     p_TMM_NMG(i) = p;
 end
 
 % compare TMM and PARS-BM
 p_TMM_BM = zeros(n, 1);
 for i = 1:n
-    [h, p] = ttest2(res_TMM(:, i), res_BM(:, i));
+    [h, p] = ttest(res_TMM(:, i), res_BM(:, i));
     p_TMM_BM(i) = p;
 end
+
+diff_TMM_BM = res_TMM-res_BM;
+boxplot(diff_TMM_BM, 'label', {'MSPE_u', 'MSPE_v', 'MAE_u', 'MAE_v', 'LogS_u',...
+    'LogS_v', 'CRPS_u', 'CRPS_v'})
