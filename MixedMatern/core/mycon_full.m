5023cffd2e5875f038c78d4e1c357dc05db45377
minjay <mjfan@ucar.edu>
2017-04-20 11:27:10 -0700

    Use nu12 instead of nu.

--- MixedMatern/core/mycon_full.m
+++ MixedMatern/core/mycon_full.m
@@ -1,12 +1,12 @@
-function [c, ceq] = mycon(beta_all)
+function [c, ceq] = mycon_full(beta_all)
 
 rho12 = beta_all(3);
 nu1 = beta_all(4);
 nu2 = beta_all(5);
-nu = (nu1+nu2)/2;
+nu12 = beta_all(6);
 d = 3;
-ub = gamma(nu1+d/2)^0.5*gamma(nu2+d/2)^0.5*gamma(nu)/gamma(nu1)^0.5/...
-    gamma(nu2)^0.5/gamma(nu+d/2);
+ub = gamma(nu1+d/2)^0.5*gamma(nu2+d/2)^0.5*gamma(nu12)/gamma(nu1)^0.5/...
+    gamma(nu2)^0.5/gamma(nu12+d/2);
 c = [rho12-ub -rho12-ub];
 ceq = [];
 
