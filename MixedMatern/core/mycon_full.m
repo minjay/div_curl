79bb4bc616e88bd635cc254397c9a54213380883
minjay <mjfan@ucar.edu>
2017-04-20 12:14:34 -0700

    (nu1+nu2)/2<=nu12

--- MixedMatern/core/mycon_full.m
+++ MixedMatern/core/mycon_full.m
@@ -7,7 +7,7 @@ nu12 = beta_all(6);
 d = 3;
 ub = gamma(nu1+d/2)^0.5*gamma(nu2+d/2)^0.5*gamma(nu12)/gamma(nu1)^0.5/...
     gamma(nu2)^0.5/gamma(nu12+d/2);
-c = [rho12-ub -rho12-ub];
+c = [rho12-ub -rho12-ub (nu1+nu2)/2-nu12];
 ceq = [];
 
 end
