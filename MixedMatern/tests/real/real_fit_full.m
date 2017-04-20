3d315cc686077b961223108e9b0b05bc5e846856
minjay <mjfan@ucar.edu>
2017-04-20 11:30:44 -0700

    Use full bivariate Matern model.

--- MixedMatern/tests/real/real_fit_full.m
+++ MixedMatern/tests/real/real_fit_full.m
@@ -1,6 +1,8 @@
 % run on server
-parpool(8)
+parpool(16)
 addpath(genpath('/home/minjay/div_curl'))
+
+% full bivariate Matern model with a1=a2=a12
 
 load('wind.mat')
 
@@ -8,15 +10,15 @@ load('wind.mat')
 [h_mat, r, P_cell, Q_cell, A_cell] = init_comp(x, y, z, n, theta, phi);
 
 % negative log-likelihood function
-negloglik1 = @(beta_all) negloglik(beta_all, h_mat, r, P_cell, Q_cell, A_cell, samples);
+negloglik1 = @(beta_all) negloglik_full(beta_all, h_mat, r, P_cell, Q_cell, A_cell, samples);
 
-% rand search
-beta_init = rand_search(negloglik1, 100, [0 0 -1 1 1 10 0 0], [10 10 1 5 5 10 10 10], true, true);
+beta_init = [0.029113 0.054503 0.281047 1.757812 2.034312 9.47189 0.210496 0.196141];
+beta_init = [beta_init(1:5) (beta_init(4)+beta_init(5))/2 beta_init(6:end)];
 
-lb = [0 0 -1 1 1 0 0 0];
-ub = [Inf Inf 1 5 5 Inf Inf Inf];
+lb = [0 0 -1 1 1 1 0 0 0];
+ub = [Inf Inf 1 5 5 5 Inf Inf Inf];
 
 % fit the model
-[beta_hat, f_min] = Matern_fit(negloglik1, beta_init, lb, ub, @mycon, true);
+[beta_hat, f_min] = Matern_fit_full(negloglik1, beta_init, lb, ub, @mycon_full, true);
 
 delete(gcp)
